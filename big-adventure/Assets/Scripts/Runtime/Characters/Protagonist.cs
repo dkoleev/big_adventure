bfdeb995a003337bc2687179db2ab1d7007a5a37
DK <dmitry.koleev@game-garden.com>
2021-08-04 14:58:36 +0300

    protagonist moving

--- big-adventure/Assets/Scripts/Runtime/Characters/Protagonist.cs
+++ big-adventure/Assets/Scripts/Runtime/Characters/Protagonist.cs
@@ -5,12 +5,14 @@ using Debug = UnityEngine.Debug;
 
 namespace Runtime.Characters {
     public class Protagonist : MonoBehaviour {
-        [SerializeField]
-        private InputReader inputReader = default;
+        [SerializeField] private InputReader inputReader = default;
+        [SerializeField] private float speedMove = 3f;
+        [SerializeField] private Transform rotateRoot;
+        [SerializeField] private float speedRotate = 5f;
 
-        private Vector2 _inputVector;
-
+        private Vector2 _inputVector = Vector2.zero;
         private GameInput _gameInput;
+        
         private void Start() {
             _gameInput = new GameInput();
             _gameInput.Player.Enable();
@@ -24,8 +26,44 @@ namespace Runtime.Characters {
             inputReader.MoveEvent -= OnMove;
         }
 
+        private void Update() {
+            Move();
+            RotateByMoveDirection();
+        }
+
         private void OnMove(Vector2 movement) {
             _inputVector = movement;
+        }
+        
+        private void Move() {
+            if (_inputVector.magnitude > 0) {
+                var speedModifier = Mathf.Clamp01(_inputVector.magnitude);
+                
+                transform.localPosition += new Vector3(
+                    _inputVector.x * Time.deltaTime * speedMove * speedModifier,
+                    0,
+                    _inputVector.y * Time.deltaTime * speedMove * speedModifier);
+            }
+        }
+
+        private void RotateByMoveDirection() {
+            if (_inputVector.magnitude < 0.001f) {
+                return;
+            }
+
+            var move = new Vector3(_inputVector.x, 0, _inputVector.y);
+            if (move.magnitude > 1f) {
+                move.Normalize();
+            }
+
+            var forward = rotateRoot.forward;
+            var angleCurrent = Mathf.Atan2( forward.x, forward.z) * Mathf.Rad2Deg;
+            var targetAngle = Mathf.Atan2(move.x, move.z) * Mathf.Rad2Deg;
+            var deltaAngle = Mathf.DeltaAngle(angleCurrent, targetAngle);
+            var targetLocalRot = Quaternion.Euler(0, deltaAngle, 0);
+            var targetRotation = Quaternion.Slerp(Quaternion.identity, targetLocalRot, speedRotate * Time.deltaTime);
+
+            rotateRoot.rotation *= targetRotation;
         }
 
         [Conditional("DEBUG")]
