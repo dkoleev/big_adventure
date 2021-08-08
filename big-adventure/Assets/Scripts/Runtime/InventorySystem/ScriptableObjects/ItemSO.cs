361fe0b130c8b2187d78eed6a5a070aa6c4c0be7
DK <dmitry.koleev@game-garden.com>
2021-08-08 07:52:16 +0300

    add localization

--- big-adventure/Assets/Scripts/Runtime/InventorySystem/ScriptableObjects/ItemSO.cs
+++ big-adventure/Assets/Scripts/Runtime/InventorySystem/ScriptableObjects/ItemSO.cs
@@ -1,35 +1,27 @@
 using System.Collections.Generic;
 using Runtime.Core;
 using UnityEngine;
+using UnityEngine.Localization;
 
 namespace Runtime.InventorySystem.ScriptableObjects {
     [CreateAssetMenu(fileName = "Item", menuName = "Inventory/Item")]
     public class ItemSO : SerializableScriptableObject {
-        /*[Tooltip("The name of the item")]
-        [SerializeField] private LocalizedString _name = default;*/
-        
-        /*[Tooltip("A description of the item")]
-        [SerializeField]
-        private LocalizedString _description = default;*/
-
+        [Tooltip("The name of the item")]
+        [SerializeField] private LocalizedString itemName = default;
+        [Tooltip("A description of the item")]
+        [SerializeField] private LocalizedString description = default;
         [Tooltip("A preview image for the item")]
-        [SerializeField]
-        private Sprite _previewImage = default;
-
+        [SerializeField] private Sprite previewImage = default;
         [Tooltip("The type of item")]
-        [SerializeField]
-        private ItemTypeSO _itemType = default;
-
+        [SerializeField] private ItemTypeSO itemType = default;
         [Tooltip("A prefab reference for the model of the item")]
-        [SerializeField]
-        private GameObject _prefab = default;
+        [SerializeField] private GameObject prefab = default;
 
-
-       // public LocalizedString Name => _name;
-       //public LocalizedString Description => _description;
-        public Sprite PreviewImage => _previewImage;
-        public ItemTypeSO ItemType => _itemType;
-        public GameObject Prefab => _prefab;
+        public LocalizedString Name => itemName;
+        public LocalizedString Description => description;
+        public Sprite PreviewImage => previewImage;
+        public ItemTypeSO ItemType => itemType;
+        public GameObject Prefab => prefab;
         public virtual List<ItemStack> IngredientsList { get;}
         public virtual ItemSO ResultingDish { get; }
 
